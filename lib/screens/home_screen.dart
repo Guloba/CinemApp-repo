616e983569bff1bf0436ca5632afdb5e85cb73a6
NatukundaPamela <86467951+NatukundaPamela@users.noreply.github.com>
2022-02-15 17:27:26 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -66,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
             IconButton(
               onPressed: () {},
               icon: const CircleAvatar(
-                backgroundColor: Colors.purple,
+                backgroundColor: Colors.white,
                 child: CircleAvatar(
                   radius: 14,
                   backgroundImage: NetworkImage(
